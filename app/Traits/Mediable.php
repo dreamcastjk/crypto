e36023f4cdebfe30a5462223089ffcde270579b5
Zhiharev <slipdoll@gmail.com>
2020-10-06 10:58:47 +0300

    added multiple media upload

--- app/Traits/Mediable.php
+++ app/Traits/Mediable.php
@@ -4,6 +4,8 @@
 namespace App\Traits;
 
 
+use Spatie\MediaLibrary\MediaCollections\Events\CollectionHasBeenCleared;
+use Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 /**
@@ -17,6 +19,7 @@ use Spatie\MediaLibrary\MediaCollections\Models\Media;
 trait Mediable
 {
     public static string $defaultCollection = 'image';  # стандартная коллекция для изображений модели.
+    public static string $productCollection = 'product';  # коллекция изображений продукта
 
     private static string $defaultThumbConversation = 'thumb';  # для получения миниатюр изображения.
 
@@ -27,6 +30,7 @@ trait Mediable
      * Устанавливаем атрибут изображения.
      *
      * @param $path
+     * @throws \Exception
      */
     public function setImageAttribute($path)
     {
@@ -40,6 +44,19 @@ trait Mediable
      */
     public function getImageAttribute()
     {
+        /* @var MediaCollection $media */
+        $media = $this->getMedia(static::$defaultCollection);
+
+        if ($media->count() > 1) {
+            $images = [];
+            /* @var Media $mediaItem */
+            foreach ($media as $mediaItem) {
+                $images[] = $mediaItem->getUrl();
+            }
+
+            return $images;
+        }
+
         return $this->getFirstMediaUrl(static::$defaultCollection);
     }
 
@@ -59,26 +76,59 @@ trait Mediable
      *
      * @param $pathValue
      * @param string $collectionName
+     * @throws \Exception
      */
     private function syncMedia($pathValue, string $collectionName)
     {
-        $this->createSingleMedia($pathValue);
+        if (empty($pathValue)) {
+            $this->clearMediaCollection($collectionName);
+            return;
+        }
+
+        $values = [];
+        if (is_string($pathValue)) {
+            $values[] = $pathValue;
+        } else {
+            $values = $pathValue;
+        }
+
+        /* @var MediaCollection $media */
+        $media = $this->getMedia($collectionName);
+
+        /* @var Media $mediaItem */
+        foreach ($media as $mediaItem) {
+            foreach ($values as $key => $value) {
+                if ($mediaItem->getUrl() == $value) {
+                    unset($values[$key]);
+                    continue 2;
+                }
+            }
+            $mediaItem->delete();
+            event(new CollectionHasBeenCleared($this, $collectionName));
+
+            if ($this->mediaIsPreloaded()) {
+                unset($this->media);
+            }
+        }
+
+        foreach ($values as $value) {
+            $this->createSingleMedia($value, $collectionName);
+        }
     }
 
     /**
      * Создание одного изображения с удалением предыдущих изображений
      *
      * @param $file
-     * @return mixed
+     * @param $collectionName
      */
-    private function createSingleMedia($file)
+    private function createSingleMedia($file, $collectionName)
     {
-        if (!$file) {
-            return $this->clearMediaCollection(static::$defaultCollection);
+        if (filter_var($file, FILTER_VALIDATE_URL)) {
+            $this->addMediaFromUrl($file)->toMediaCollection($collectionName);
+        } else {
+            $this->addMedia($file)->toMediaCollection($collectionName);
         }
-
-        $this->clearMediaCollection(static::$defaultCollection);
-        return $this->addMedia($file)->toMediaCollection(static::$defaultCollection);
     }
 
     /**
